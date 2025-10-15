7b00d0e1b175b0673fe5ac156a4f9412ad38030f
asadaftab667 <asadaftab667@gmail.com>
2025-10-15 22:19:29 +0500

    fix

--- src/Api/Components/hotelBed/hotelBed.repository.ts
+++ src/Api/Components/hotelBed/hotelBed.repository.ts
@@ -476,13 +476,10 @@ export default class HotelBedFileRepo {
             batchAggregated[section] = [];
           }
           
-          const now = new Date().toISOString().slice(0, 19).replace('T', ' ');
           const mappedRows = mapRow(section, rows).map((r: any) => ({
             id: randomUUID(),
             hotelBedId: fileId,
             ...r,
-            createdAt: now,
-            updatedAt: now,
           }));
           
           batchAggregated[section].push(...mappedRows);
